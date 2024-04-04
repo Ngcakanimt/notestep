432a12708ff838e3c7432f6945c935392b17a09a
Mthawelanga <mthangcakani2@gmail.com>
2024-04-04 22:22:00 +0200

    Pricing: Changed hero text

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -79,7 +79,7 @@ const page = async () => {
       <MaxWidthWrapper className="mb-8 mt-24 text-center max-w-5xl">
         <div className="mx-auto mb-10 sm:max-w-lg">
           <h1 className="text-6xl font-bold sm:text-7xl">
-            Ctrl + F is overrated. Let us help you out.
+            Ctrl + F is overrated.
           </h1>
           <p className="mt-5 text-gray-600 sm:text-lg">
             Whether you&apos;re trying out our service or you need more,
